8869f321ccae47c5997ea27235e94e0a01116ef0
Thomas <thomas.blakely@gmx.com>
2021-09-20 20:45:46 +1200

    test #1

---
+++ bj_declare.h
@@ -0,0 +1,12 @@
+// Ifis not included to any file
+// Include guard
+#ifndef BJ_DECLARE_H
+
+// Declare macro as a flag that specifies arith.h is included
+#define BJ_DECLARE_H
+
+// Declare all functions
+int paint_card (int x, int y);
+int move_counter (int x, int y);
+
+#endif
--- main.c
+++ main.c
@@ -2,9 +2,12 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
+#include "bj_declare.h"
+
 int main(void)
 {
-
+  printf("paint_card(10, 20)  = %.2f\n", paint_card(10, 20));
+  printf("move_counter(20, 20)  = %.2f\n", move_counter(20, 20));
 
   return 0;
 }
